9de474605e46e446267e6e62b3a7bac3530b524e
Ian-Richard <ian.fiddeman@gmail.com>
2021-12-05 22:50:14 +0000

    useState added for addgoal and input handlers

--- App.js
+++ App.js
@@ -1,33 +1,72 @@
-import React from "react";
-import { StyleSheet, Text, TextInput, View, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+  };
+
   return (
-   
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.input} />
-        <Button title="Add" />
+        <TextInput
+          placeholder="Goals"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="Add" onPress={addGoalHandler} />
       </View>
-      <View />
+      <ScrollView>
+        <View>
+          {courseGoals.map((goal, index) => (
+            <View key={index} style={styles.listItem}>
+              <Text>
+                {index + 1}. {goal}
+              </Text>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
     </View>
-    
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 50
+    padding: 50,
   },
   inputContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
   },
-  input: { 
-    width: "80%", 
-    borderColor: "black", 
-    borderWidth: 1, 
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
     padding: 10,
-   },
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
+  },
 });
