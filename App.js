32805387cb8fa13fecf7f2ca491a52d3a90236ea
Ian-Richard <ian.fiddeman@gmail.com>
2021-12-06 17:42:31 +0000

    validation added to title length on addgoalhandler

--- App.js
+++ App.js
@@ -1,10 +1,5 @@
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  View,
-  Button,
-  FlatList,
-} from "react-native";
+import { StyleSheet, View, Button, FlatList } from "react-native";
 
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
@@ -12,8 +7,13 @@ import GoalInput from "./components/GoalInput";
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
   const [isAddMode, setIsAddMode] = useState(false);
+  console.log("Re-rendering component");
+  console.log(courseGoals);
 
   const addGoalHandler = (goalTitle) => {
+    if (goalTitle.length === 0) {
+      return;
+    }
     setCourseGoals((currentGoals) => [
       ...currentGoals,
       { id: Math.random().toString(), value: goalTitle },
@@ -22,23 +22,26 @@ export default function App() {
   };
 
   removeGoalHandler = (goalId) => {
+    console.log("to delete: ", goalId);
+    console.log("course goals array of objs", courseGoals);
     setCourseGoals((currentGoals) => {
-      return currentGoals.filter((goal) => goal.id !== goalId)});
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
   };
 
   const cancelGoalAdditionHandler = () => {
     setIsAddMode(false);
-  }
+  };
 
   return (
     <View style={styles.screen}>
       <Button title="Add New Goal" onPress={() => setIsAddMode(true)} />
-      <GoalInput 
-        visible={isAddMode} 
+      <GoalInput
+        visible={isAddMode}
         onAddGoal={addGoalHandler}
         onCancel={cancelGoalAdditionHandler}
-        />
-      
+      />
+
       <FlatList
         keyExtractor={(item, index) => item.id}
         data={courseGoals}
